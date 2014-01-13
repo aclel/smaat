f0963b7ccc72e923d59038bdd129eb9e6862a7a2
iamwinrar <andrew.cleland3@gmail.com>
2014-01-13 19:25:05 +1000

    Added authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Smaat::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/parents"
-  get "static_pages/students"
+  devise_for :users
+  root "static_pages#home"
+  match '/parents', to: "static_pages#parents", via: 'get'
+  match '/students', to: "static_pages#students", via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
