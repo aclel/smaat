54c827fe9eebdacaafaf78585a370a058c8ab44e
iamwinrar <andrew.cleland3@gmail.com>
2014-01-15 19:05:35 +1000

    Confirmation is working.

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Smaat::Application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-   :tls => true,
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :domain => "gmail.com",
-   :authentication => :login,
-   :user_name => "andrew.cleland3",
-   :password => "Csandwich1"
- }
+
+
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,18 @@ Smaat::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://smaat.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp 
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "http://smaat.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
