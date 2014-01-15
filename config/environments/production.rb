07909cbddaf293574c21082aab5e2b98ad150383
iamwinrar <andrew.cleland3@gmail.com>
2014-01-15 19:10:44 +1000

    Changed email settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,12 @@ Smaat::Application.configure do
   config.action_mailer.delivery_method = :smtp 
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "http://smaat.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :address: "smtp.gmail.com",
+    :port: 587,
+    :domain: "http://smaat.herokuapp.com",
+    :authentication: "plain",
+    :enable_starttls_auto: true,
+    :user_name: ENV["GMAIL_USERNAME"],
+    :password: ENV["GMAIL_PASSWORD"]
   }
 end
