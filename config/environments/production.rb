fac5637dadc1b0e31835e81680d3ad19a5c7a4d4
iamwinrar <andrew.cleland3@gmail.com>
2014-01-19 11:05:34 +1000

    Trying to get sign_up working again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,17 @@ Smaat::Application.configure do
   #   user_name: "andrew.cleland3@gmail.com",
   #   password: "Csandwich1"
   # }
+
+   config.action_mailer.default_url_options = {:host => 'smaat.herokuapp.com', :protocol => 'http'} 
+   config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :authentication => :plain,   
+     :enable_starttls_auto => true,  
+     :user_name => 'andrew.cleland3@gmail.com',
+     :password => 'Csandwich1'
+   }
 end
