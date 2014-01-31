f795d576e5aeb26676410c9a7bf7bf71c3966f17
iamwinrar <andrew.cleland3@gmail.com>
2014-01-31 17:51:00 +1000

    Fixed dropdown.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_nested_form
+
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery_nested_form
 //= require turbolinks
 //= require_tree .
