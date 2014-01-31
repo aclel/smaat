eec7bec5ce2db2ae725f64ee8214b15f24ff0baa
iamwinrar <andrew.cleland3@gmail.com>
2014-01-31 14:15:05 +1000

    Changed mail settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Smaat::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  #config.action_mailer.default_url_options = { :host => 'smaat.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'smaat.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
 
