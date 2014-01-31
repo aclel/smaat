4fe09d32f23588ae5dcca2c45d8e6c6de70b74e9
iamwinrar <andrew.cleland3@gmail.com>
2014-01-31 22:14:39 +1000

    Reverted email changes.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
   def after_confirmation_path_for(resource_name, resource)
 			after_sign_in_path_for(resource)
-      SignUpInfoMailer.sign_up_information(current_user).deliver
+      SignUpInfoMailer.sign_up_information.deliver
   end
-
 end
--- app/mailers/sign_up_info_mailer.rb
+++ app/mailers/sign_up_info_mailer.rb
@@ -1,8 +1,7 @@
 class SignUpInfoMailer < ActionMailer::Base
   default from: "andrew.cleland3@gmail.com"
 
-  def sign_up_information(user)
-    @user = user
+  def sign_up_information
     mail to: user.email, subject: "SMAAT Sign Up Information"
   end
 end
