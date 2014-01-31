42a94248892935e10a1851a9e1e70e979c1ccf29
iamwinrar <andrew.cleland3@gmail.com>
2014-01-31 21:39:51 +1000

    Changed after_confirmation_path_for

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,10 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
   def after_confirmation_path_for(resource_name, resource)
-      if signed_in?
-        signed_in_root_path(resource)
-      else
-        new_session_path(resource_name)
-      end
+			after_sign_in_path_for(resource)
       SignUpInfoMailer.sign_up_information.deliver
   end
 end
