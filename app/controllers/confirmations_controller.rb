94b563a1d67674a134a65651f03d0e14188e9488
iamwinrar <andrew.cleland3@gmail.com>
2014-02-01 20:10:07 +1000

    Email stuff.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,7 +2,6 @@ class ConfirmationsController < Devise::ConfirmationsController
 	rescue_from Exception, :with => :redirect
   def after_confirmation_path_for(resource_name, resource)
 			super
-      NotificationMailer.notify(@user).deliver
       SignUpInfoMailer.sign_up_information(@user).deliver
   end
 
@@ -10,5 +9,6 @@ class ConfirmationsController < Devise::ConfirmationsController
 		def redirect
 			redirect_to root_path
 			flash[:notice] = "Your account has been confirmed. An email has been sent which contains important registration information."
+      NotificationMailer.notify(@user).deliver
 		end
 end
