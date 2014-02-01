64dab007a1be3b66d4b8de9eea2f2aa73bac784e
iamwinrar <andrew.cleland3@gmail.com>
2014-02-01 18:32:54 +1000

    Changed to text email.

--- app/mailers/sign_up_info.rb
+++
@@ -1,7 +0,0 @@
-class SignUpInfoMailer < ActionMailer::Base
-  default from: "andrew.cleland3@gmail.com"
-
-  def sign_up_information
-    mail to: "andrew.cleland3@gmail.com", subject: "SMAAT Sign Up Information"
-  end
-end
