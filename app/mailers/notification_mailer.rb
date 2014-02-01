5bc744d1451348860af9725a2597f696fd100c39
iamwinrar <andrew.cleland3@gmail.com>
2014-02-01 20:37:25 +1000

    Changed to Nelson's email.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,6 +3,6 @@ class NotificationMailer < ActionMailer::Base
 
   def notify(user)
     @user = user
-    mail to: "andrew.cleland3@gmail.com", subject: user.first_name + user.last_name + " - Another SMAAT Minion has joined the ranks..."
+    mail to: "smaatenquiries@outlook.com", subject: user.first_name + user.last_name + " - Another SMAAT Minion has joined the ranks..."
   end
 end
