fa48f750ebcd21dd788f9d3a8521f7b4967d45f8
iamwinrar <andrew.cleland3@gmail.com>
2014-02-01 18:38:49 +1000

    Added attachments.

--- app/mailers/sign_up_info_mailer.rb
+++ app/mailers/sign_up_info_mailer.rb
@@ -2,9 +2,9 @@ class SignUpInfoMailer < ActionMailer::Base
   default from: "smaatenquiries@outlook.com"
 
   def sign_up_information(user)
-  	#attachments['parent_student_agreement.pdf'] = File.read(Rails.root.join("app/assets/attachments/parent_student_agreement.pdf"))
-  	#attachments['payment_detail_form.pdf'] = File.read(Rails.root.join("app/assets/attachments/payment_detail_form.pdf"))
-  	#attachments['term1_notice.pdf'] = File.read(Rails.root.join("app/assets/attachments/term1_notice.pdf"))
+  	attachments['parent_student_agreement.pdf'] = File.read(Rails.root.join("app/assets/attachments/parent_student_agreement.pdf"))
+  	attachments['payment_detail_form.pdf'] = File.read(Rails.root.join("app/assets/attachments/payment_detail_form.pdf"))
+  	attachments['term1_notice.pdf'] = File.read(Rails.root.join("app/assets/attachments/term1_notice.pdf"))
     mail to: user.email, subject: "SMAAT Registration Information"
   end
 end
