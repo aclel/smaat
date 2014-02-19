6839428737bc8c74a32bb0b664f066dff287bd56
Andrew Cleland <andrew@Andrews-Mac-Pro.local>
2014-02-19 12:09:54 +1000

    Updated attachments, removed auto sign in.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,23 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create	
-  	super
+  	build_resource(sign_up_params)
+
+    if resource.save
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+
   	# send sign up info to parent
   	SignUpInfoMailer.sign_up_information(@user).deliver
   	# send email to admin
