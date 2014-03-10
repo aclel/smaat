65d4547277ef68534acb000a8b223c2feec59c7c
aclel <andrew.cleland3@gmail.com>
2014-03-10 12:22:21 +1000

    Added term2_notice

--- app/mailers/sign_up_info_mailer.rb
+++ app/mailers/sign_up_info_mailer.rb
@@ -6,6 +6,7 @@ class SignUpInfoMailer < ActionMailer::Base
   	attachments['parent_student_agreement.pdf'] = File.read(Rails.root.join("app/assets/attachments/parent_student_agreement.pdf"))
   	attachments['payment_detail_form.pdf'] = File.read(Rails.root.join("app/assets/attachments/payment_detail_form.pdf"))
   	attachments['term1_notice.pdf'] = File.read(Rails.root.join("app/assets/attachments/term1_notice.pdf"))
+  	attachments['term2_notice_2014.pdf'] = File.read(Rails.root.join("app/assets/attachments/term2_notice_2014.pdf"))
     mail to: user.email, subject: "SMAAT Registration Information"
   end
 end
