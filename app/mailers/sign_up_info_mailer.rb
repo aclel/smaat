7f35d7cd9c9abf03ad2e50be4a44e459b798320d
aclel <andrew.cleland3@gmail.com>
2015-08-01 15:59:28 +1000

    Update for term 3 2015
    - Change attachments
    - Add Prahlad's name to everything

--- app/mailers/sign_up_info_mailer.rb
+++ app/mailers/sign_up_info_mailer.rb
@@ -5,7 +5,7 @@ class SignUpInfoMailer < ActionMailer::Base
   	@user = user
   	attachments['parent_student_agreement.pdf'] = File.read(Rails.root.join("app/assets/attachments/parent_student_agreement.pdf"))
   	attachments['payment_detail_form.pdf'] = File.read(Rails.root.join("app/assets/attachments/payment_detail_form.pdf"))
-  	attachments['term2_notice_2015.pdf'] = File.read(Rails.root.join("app/assets/attachments/term2_notice_2015.pdf"))
+  	attachments['term3_notice_2015.pdf'] = File.read(Rails.root.join("app/assets/attachments/term3_notice_2015.pdf"))
     mail to: user.email, subject: "SMAAT Registration Information"
   end
 end
